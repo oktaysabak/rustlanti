0d563834538d1e40dd4dda953946fb3a23704917
oktaysabak1 <oktay.sabak@kreatize.com>
2021-04-20 02:06:06 +0300

    new code readings

--- src/main.rs
+++ src/main.rs
@@ -58,6 +58,19 @@ fn main() {
     for item in 0..5 {
         println!("{}", item * 2);
     }
+    let fruits = vec!["banana", "apple", "coconut", "orange", "strawberry"];
+
+    // pick the first item:
+    let first = fruits.get(0);
+    println!("{:?}", first);
+
+    // pick the third item:
+    let third = fruits.get(2);
+    println!("{:?}", third);
+
+    // pick the 99th item, which is non-existent:
+    let non_existent = fruits.get(99);
+    println!("{:?}", non_existent);
 }
 
 fn another_function() {
